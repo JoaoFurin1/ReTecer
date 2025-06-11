5d47d9bc1b8591725d1f87f3e3671bf98f842a61
João Furini <joao78furini@gmail.com>
2025-06-11 16:42:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerText = 'Obrigado por querer ajudar o nosso projeto! \nFale conosco pelo instagram: projeto.retecer 💚';
+  }
+  
